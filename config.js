448341067438017928db797ade811d2af9b32631
codeqihanstatus <117992212+codeqihanstatus@users.noreply.github.com>
2022-11-11 20:44:19 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '启涵的所有内容运行状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793028803-75c6998e690f2f230eec0a7a',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://codeqihanstatus.github.io'
+    },
+    {
+      text: '启涵的博客',
+      url: 'https://www.codeqihan.com'
+    },
+  ],
+};
